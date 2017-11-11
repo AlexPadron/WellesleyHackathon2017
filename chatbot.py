0c3352b5ecf65be9f7b03d192223da106f6aec48
Ziyu <ziyu_zoo@tongwandou-2.local>
2017-11-11 11:07:15 -0500

    Added bot and messages in setting

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,24 @@
+from chatterbot import ChatBot
+
+# Initializing ChatBot
+chatbot = ChatBot(
+    'Ron Obvious',
+    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+)
+
+# Train based on the english corpus
+chatbot.train("chatterbot.corpus.english")
+
 def reply_to_msg(msg):
-    return 'hello world'
+	'''Bot that responds'''
+    return chatbot.get_response(msg)
 
 def get_initial_msg(number, username):
-    return 'foo'
+	'''
+	Initiates conversation with user by going in sequence of predefined chats
+	'''
+	messages = []
+	num = 0
+	while num< len(messages):
+		yield messages[num]
+		num += 1
